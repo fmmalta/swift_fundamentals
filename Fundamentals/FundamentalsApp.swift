1589f303c7536eb89db8366280cf72f8b78f46b4
fmmalta <fellipedmmalta@gmail.com>
2023-04-18 18:59:20 -0300

    First commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Fellipe Malta on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
